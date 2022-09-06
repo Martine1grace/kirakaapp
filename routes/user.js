17b14cfd897af97080d1618b1da6aecfc64545f7
Martine1grace <martinegrace16@gmail.com>
2022-09-06 09:09:53 +0200

    first commit

---
+++ routes/user.js
@@ -0,0 +1,28 @@
+const express = require("express");
+ app = express();
+
+const {
+  getAllUser,
+  createUser,
+  getOneUser,
+  deleteOneUser,
+  updateUser,
+  login,
+} = require("../controllers/user");
+const { protected, authorize, logout } = require("../middlewares/auth");
+const routes = express.Router();
+
+routes
+  .route("/:_id")
+  .get( getOneUser)
+  .delete( deleteOneUser)
+  .patch( updateUser);
+routes
+  .route("/")
+  .get( getAllUser)
+  .post(createUser);
+
+routes.route("/login").post(login);
+routes.route("/user/logout").get(logout);
+
+module.exports = routes;
